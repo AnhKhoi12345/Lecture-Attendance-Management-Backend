3821511de45ddd68efb1c195617f1b8bb5628dde
khoi <hnak2442001smg4@gmail.com>
2024-07-20 20:40:19 +0700

    Fix get lecturer and student module route

--- src/routes/getStudentModule.js
+++ src/routes/getStudentModule.js
@@ -10,7 +10,7 @@ export const getStudentRoute={
         const userId = req.params.userId;
         if (user.user_id !== userId) throw unauthorized('no auth');
         const {results} = await db.query(
-            'select distinct module.module_id, module.name,lecturer.name as lecturer, semester.sem_type as semester, semester.start_date, semester.end_date from lam inner join (module_enroll, student, module, lecturer, semester, program_modules) on (lam.acc_id = ? and lam.school_id = module_enroll.student_id and module_enroll.student_id = student.student_id and module_enroll.module_id = module.module_id and module.lecturer_id = lecturer.staff_id and module.module_id = program_modules.module_id and program_modules.sem_id = semester.sem_id) order by start_date;', [userId]
+            'select distinct module.module_id, module.name,program_modules.program_id,lecturer.name as lecturer,program_modules.intake, semester.sem_type as semester, semester.start_date, semester.end_date from lam inner join (module_enroll, student, module, lecturer, semester, program_modules) on (lam.acc_id = ? and lam.school_id = module_enroll.student_id and module_enroll.student_id = student.student_id and module_enroll.module_id = module.module_id and module.lecturer_id = lecturer.staff_id and module.module_id = program_modules.module_id and program_modules.sem_id = semester.sem_id) order by start_date;', [userId]
         );
         if(!results)throw notFound("Module not found")
         return results;
